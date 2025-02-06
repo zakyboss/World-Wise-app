4ac5074f5fbfecb6da6511b6fcc338afd73f17b9
Zakyboss <Boochimo8@gmail.com>
2025-02-06 11:13:03 +0300

    Progress

---
+++ world-wise/src/Components/AppNav.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+import styles from "/src/Components/AppNav.module.css";
+export default function AppNav() {
+  return <nav className={styles.nav}>App Navigation</nav>;
+}
---
+++ world-wise/src/Components/PageNav.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import styles from "/src/Components/PageNav.module.css";
+export default function PageNav() {
+  return (
+    <nav className={styles.nav}>
+      <ul>
+        <li>
+          <NavLink to="/">Home</NavLink>
+        </li>
+        <li>
+          <NavLink to="/pricing">Pricing</NavLink>
+        </li>
+        <li>
+          <NavLink to="/product">Product</NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+}
