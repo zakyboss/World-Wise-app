9e6299e3714278cbaa0243d935d62b1732817d80
Zakyboss <Boochimo8@gmail.com>
2025-02-11 21:28:02 +0300

    Rendering using contexts instead of props and passing data to components using context.provider

--- world-wise/src/App.jsx
+++ world-wise/src/App.jsx
@@ -6,63 +6,35 @@ import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./pages/AppLayout";
 import Login from "./pages/Login";
 import CityList from "./Components/CityList";
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
+import { CitiesProvider } from "./contexts/CitiesContext";
 import CountriesList from "./Components/CountriesList";
 import City from "./Components/City";
 import Form from "./Components/Form";
-import FetchData from "./Components/FetchData";
 export default function App() {
-  const [cities, setCities] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  useEffect(() => {
-    async function getCities() {
-      try {
-        setIsLoading(true);
-        const res = await fetch("http://localhost:8000/cities");
-
-        if (!res.ok) {
-          throw new Error(`Server Error: ${res.status} ${res.statusText}`);
-        }
-
-        const data = await res.json();
-        // console.log(data);
-        setCities(data);
-      } catch (err) {
-        console.error("Error fetching cities:", err.message);
-      } finally {
-        setIsLoading(false);
-      }
-    }
-
-    getCities();
-  }, []);
   return (
     <div>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="pricing" element={<Pricing />} />
-          <Route path="product" element={<Product />} />
-          <Route path="login" element={<Login />} />
-          <Route path="data" element={<FetchData />} />
-          <Route path="*" element={<PageNotFound />} />
+      <CitiesProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="pricing" element={<Pricing />} />
+            <Route path="product" element={<Product />} />
+            <Route path="login" element={<Login />} />
+            <Route path="*" element={<PageNotFound />} />
 
-          {/* Define "app" as the parent route */}
-          <Route path="app" element={<AppLayout />}>
-            <Route index element={<Navigate replace to="cities" />} />
-            <Route
-              path="cities"
-              element={<CityList cities={cities} isLoading={isLoading} />}
-            />
-            <Route path="cities/:id" element={<City />} />
-            <Route
-              path="countries"
-              element={<CountriesList cities={cities} isLoading={isLoading} />}
-            />
-            <Route path="form" element={<Form />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+            {/* Define "app" as the parent route */}
+            <Route path="app" element={<AppLayout />}>
+              <Route index element={<Navigate replace to="cities" />} />
+              <Route path="cities" element={<CityList />} />
+              <Route path="cities/:id" element={<City />} />
+              <Route path="countries" element={<CountriesList />} />
+
+              <Route path="form" element={<Form />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </CitiesProvider>
     </div>
   );
 }
