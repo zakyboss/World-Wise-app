6ee71da9e5338d9861175df5f527e1b026f4fccf
Zakyboss <Boochimo8@gmail.com>
2025-02-11 21:22:16 +0300

    Loading individual city

--- world-wise/src/Components/City.jsx
+++ world-wise/src/Components/City.jsx
@@ -1,6 +1,9 @@
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import styles from "./City.module.css";
-
+import Spinner from "./Spinner";
+import Button from "./Button";
+import { useCities } from "../contexts/CitiesContext";
+import { useEffect } from "react";
 const formatDate = (date) =>
   new Intl.DateTimeFormat("en", {
     day: "numeric",
@@ -10,56 +13,67 @@ const formatDate = (date) =>
   }).format(new Date(date));
 
 function City() {
-  // TEMP DATA
+  const navigate = useNavigate();
   const { id } = useParams();
-  console.log(id);
-  const currentCity = {
-    cityName: "Lisbon",
-    emoji: "🇵🇹",
-    date: "2027-10-31T15:59:59.138Z",
-    notes: "My favorite city so far!",
-  };
-
+  const { getCity, currentCity, isLoading } = useCities();
+  useEffect(
+    function () {
+      getCity(id);
+    },
+    [id]
+  );
   const { cityName, emoji, date, notes } = currentCity;
 
-  return (
-    <h1> City {id}</h1>
-    // <div className={styles.city}>
-    //   <div className={styles.row}>
-    //     <h6>City name</h6>
-    //     <h3>
-    //       <span>{emoji}</span> {cityName}
-    //     </h3>
-    //   </div>
+  console.log(currentCity);
+  if (isLoading) {
+    return <Spinner />;
+  } else {
+    return (
+      <div className={styles.city}>
+        <div className={styles.row}>
+          <h6>{currentCity.cityName}</h6>
+          <h3>
+            <span>{emoji}</span> {cityName}
+          </h3>
+        </div>
 
-    //   <div className={styles.row}>
-    //     <h6>You went to {cityName} on</h6>
-    //     <p>{formatDate(date || null)}</p>
-    //   </div>
+        <div className={styles.row}>
+          <h6>You went to {cityName} on</h6>
+          <p>{formatDate(date || null)}</p>
+        </div>
 
-    //   {notes && (
-    //     <div className={styles.row}>
-    //       <h6>Your notes</h6>
-    //       <p>{notes}</p>
-    //     </div>
-    //   )}
+        {notes && (
+          <div className={styles.row}>
+            <h6>Your notes</h6>
+            <p>{notes}</p>
+          </div>
+        )}
 
-    //   <div className={styles.row}>
-    //     <h6>Learn more</h6>
-    //     <a
-    //       href={`https://en.wikipedia.org/wiki/${cityName}`}
-    //       target="_blank"
-    //       rel="noreferrer"
-    //     >
-    //       Check out {cityName} on Wikipedia &rarr;
-    //     </a>
-    //   </div>
+        <div className={styles.row}>
+          <h6>Learn more</h6>
+          <a
+            href={`https://en.wikipedia.org/wiki/${cityName}`}
+            target="_blank"
+            rel="noreferrer"
+          >
+            Check out {cityName} on Wikipedia &rarr;
+          </a>
+        </div>
 
-    //   <div>
-    //     <ButtonBack />
-    //   </div>
-    // </div>
-  );
+        <div>
+          {" "}
+          <Button
+            type="back"
+            onClick={() => {
+              navigate(-1);
+            }}
+          >
+            &larr; Back
+          </Button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default City;
