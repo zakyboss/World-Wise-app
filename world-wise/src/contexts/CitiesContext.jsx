acf74c1de943fef604681b63aed23d1a156a809b
Zakyboss <Boochimo8@gmail.com>
2025-02-15 15:08:33 +0300

    Working with delete and add city logic

--- world-wise/src/contexts/CitiesContext.jsx
+++ world-wise/src/contexts/CitiesContext.jsx
@@ -48,9 +48,64 @@ function CitiesProvider({ children }) {
       setIsLoading(false);
     }
   }
+  async function addCity(newCity) {
+    if (!newCity) return;
 
+    // Check if the city already exists (case insensitive)
+    if (
+      cities.some(
+        (city) => city.cityName.toLowerCase() === newCity.cityName.toLowerCase()
+      )
+    ) {
+      return;
+    }
+
+    try {
+      const res = await fetch("http://localhost:8000/cities", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newCity),
+      });
+
+      if (!res.ok) {
+        throw new Error(`Server Error: ${res.status} ${res.statusText}`);
+      }
+
+      const data = await res.json();
+
+      // Update state with the new city
+      setCities((cities) => [...cities, data]);
+    } catch (err) {
+      console.error("Error adding city:", err.message);
+    }
+  }
+  async function deleteCity(id) {
+    try {
+      setIsLoading(true);
+      const res = await fetch(`http://localhost:8000/cities/${id}`, {
+        method: "DELETE",
+      });
+
+      if (!res.ok) {
+        throw new Error(`Server Error: ${res.status} ${res.statusText}`);
+      }
+
+      // const data = await res.json();
+
+      // Update state with the new city
+      setCities((cities) => cities.filter((city) => city.id !== id));
+    } catch (err) {
+      console.error("Error deleting city:", err.message);
+    } finally {
+      setIsLoading(false);
+    }
+  }
   return (
-    <CitiesContext.Provider value={{ cities, isLoading, currentCity, getCity }}>
+    <CitiesContext.Provider
+      value={{ cities, isLoading, currentCity, getCity, addCity, deleteCity }}
+    >
       {children}
     </CitiesContext.Provider>
   );
