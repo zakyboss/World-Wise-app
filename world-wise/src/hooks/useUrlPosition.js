0fc166e020e458f83f021209036a3ca4196dd60f
Zakyboss <Boochimo8@gmail.com>
2025-02-15 15:05:05 +0300

    Hooks added

---
+++ world-wise/src/hooks/useUrlPosition.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { useSearchParams } from 'react-router-dom';
+
+export function useUrlPosition() {
+    const [searchParams] = useSearchParams();
+    const lat = searchParams.get("lat");
+    const lng = searchParams.get("lng");
+  
+    return {lat , lng}
+  
+}
